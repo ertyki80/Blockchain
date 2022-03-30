747dace15480da4e4840d77731fcce4b1d37d7e2
Ivan Kihtan <42695780+ertyki80@users.noreply.github.com>
2022-03-31 02:18:31 +0300

    BC-03 Fix test, add console writer

--- Blockchain.Console/Program.cs
+++ Blockchain.Console/Program.cs
@@ -1,32 +1,55 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Blockchain.Bll.Implementation;
 using Blockchain.Common.Models;
+using Newtonsoft.Json;
 
 namespace Blockchain.Console
 {
     class Program
     {
-        /*static void Test_GenerateBlockWithStartingZeroN_BlockChain()
+        static void Test_GenerateBlockWithStartingZeroN_BlockChain()
         {
             for (int i = 0; i < 20; i++)
             {
                 System.Console.WriteLine($"For starting zero : {i}");
-                var watch = System.Diagnostics.Stopwatch.StartNew();
-                Person personOne = new Person("Alice", 200);
-                Person personSecond = new Person("Bob", 200);
-                BlockChainService blockChainService = new BlockChainService(i);
-                blockChainService.CreateTransaction(ref personOne, ref personSecond, 10.2);
-                blockChainService.CreateTransaction(ref personSecond, ref personOne, 22.2);
-                blockChainService.CreateTransaction(ref personOne, ref personSecond, 12.1);
-                blockChainService.CreateTransaction(ref personSecond, ref personOne, 3.2);
-                Block block = blockChainService.GenerateBlock();
+                //Service set up
+                TransactionService transactionService = new TransactionService();
+                BlockChainService blockChainService = new BlockChainService(2);
+            
+                //Persons:
+                Person personOne = new Person("Alice",200);
+                Person personSecond = new Person("Bob",200);
+            
+                // Send and received money:
+                List<Transaction> transactions = new List<Transaction>();
+            
+                //BLOCK 1
+                transactions.Add(transactionService.CreateTransaction( personOne,  personSecond, 31.2));
+                transactions.Add(transactionService.CreateTransaction( personOne,  personSecond, 11.1));
+                
+                //Track time generation
+                Stopwatch watch = Stopwatch.StartNew();
+                Block block = blockChainService.GenerateBlock(transactions);
                 watch.Stop();
+                
+                PrintBlock(block);
                 var elapsedMs = watch.Elapsed.TotalMilliseconds;
                 System.Console.WriteLine($"Total Ms: {elapsedMs}");
             }
             //blockChainService.SaveBlockChain("Test.json");
-        }*/
-        
+        }
+
+        static void PrintBlock(Block block)
+        {
+            System.Console.WriteLine("Block:");
+            System.Console.WriteLine($"Block hash: {block.Hash.Value}");
+            System.Console.WriteLine($"Block nonce: {block.Nonce}");
+            System.Console.WriteLine($"Block number: {block.Number}");
+            System.Console.WriteLine($"Block Transaction: \n{JsonConvert.SerializeObject(block.Transaction)}");
+            System.Console.WriteLine($"Previously Hash: {block.PreviousHash.Value}");
+            System.Console.WriteLine($"Previously Block: [Too long... Check previously hash]");
+        }
         
         static void Test_GenerateAndSave2Blocks_BlockChain()
         {
@@ -45,12 +68,14 @@ namespace Blockchain.Console
             transactions.Add(transactionService.CreateTransaction( personOne,  personSecond, 31.2));
             transactions.Add(transactionService.CreateTransaction( personOne,  personSecond, 11.1));
             Block block1 = blockChainService.GenerateBlock(transactions);
+            PrintBlock(block1);
             // END BLOCK 1
             
             //BLOCK 2
             transactions.Add(transactionService.CreateTransaction( personSecond,  personOne, 10.0));
             transactions.Add(transactionService.CreateTransaction( personOne,  personSecond, 20.0));
             Block block2 = blockChainService.GenerateBlock(transactions,block1);
+            PrintBlock(block2);
             // END BLOCK 2
             
             blockChainService.SaveBlockChain(block2,"Test.json");
